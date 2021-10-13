4e89c9f55c05287cc1781161e80eb86102c98a04
Rowanmh <rowanmh@gmail.com>
2021-10-13 20:37:48 +1100

    use sciris parallelize for parallelizing

--- code/cambodia/run_all_cambodia.py
+++ code/cambodia/run_all_cambodia.py
@@ -4,24 +4,21 @@
 @author: RIH
 """
 
-
 if __name__ == '__main__':
-    import numpy as np
-    import multiprocessing
-    np.random.seed(100) #consistency of results
-    
+    # import multiprocessing
+    import sciris as sc
     from run_cambodia import run_me
     
-    book_key_all = ['Pursat_low', 'Pursat_high', 'Mondul_Kiri_low', 'Mondul_Kiri_high', 'Kampong_Chhnang_low', 'Kampong_Chhnang_high', 'Battambang_low', 'Battambang_high', 'Takeo_low', 'Takeo_high', 'Pailin_low', 'Pailin_high']
+    book_key_all = ['Pursat_low', 'Pursat_high', 'Mondul_Kiri_low', 'Mondul_Kiri_high', 'Kampong_Chhnang_low', 'Kampong_Chhnang_high'] #, 'Battambang_low', 'Battambang_high', 'Takeo_low', 'Takeo_high', 'Pailin_low', 'Pailin_high']
     
     # book_key_all = ['Pailin_low', 'Pailin_high']
     
     global book_key
     
-    
     # run the model for all provinces, for low and high baseline incidences
-    with multiprocessing.Pool() as pool:
-        pool.map(run_me, book_key_all)
+    sc.parallelize(run_me, book_key_all)
+    # with multiprocessing.Pool() as pool:
+    #     pool.map(run_me, book_key_all)
     # for prov in book_key_all:
     #     run_me(prov)
     
@@ -29,5 +26,5 @@ if __name__ == '__main__':
     # aggregate and plot the results (shown in paper)
     from result_aggregation import run_post_processing
     
-    run_post_processing(flag_all_prevalences = True, flag_resort_data = False, flag_make_plots = True, flag_elimination_comparison = True)
+    run_post_processing(flag_make_plots = True, flag_elimination_comparison = True)
 
