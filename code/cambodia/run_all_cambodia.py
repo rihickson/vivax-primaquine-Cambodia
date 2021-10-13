51477cbecf0e1eeb4915214a9e892f720a3adb70
Rowanmh <rowanmh@gmail.com>
2021-10-13 20:39:45 +1100

    Update run_all_cambodia.py

--- code/cambodia/run_all_cambodia.py
+++ code/cambodia/run_all_cambodia.py
@@ -5,11 +5,12 @@
 """
 
 if __name__ == '__main__':
-    # import multiprocessing
     import sciris as sc
     from run_cambodia import run_me
     
-    book_key_all = ['Pursat_low', 'Pursat_high', 'Mondul_Kiri_low', 'Mondul_Kiri_high', 'Kampong_Chhnang_low', 'Kampong_Chhnang_high'] #, 'Battambang_low', 'Battambang_high', 'Takeo_low', 'Takeo_high', 'Pailin_low', 'Pailin_high']
+    #split into two for memory constraints
+    book_key_all = ['Pursat_low', 'Pursat_high', 'Mondul_Kiri_low', 'Mondul_Kiri_high', 'Kampong_Chhnang_low', 'Kampong_Chhnang_high'] 
+    book_key_second_half = ['Battambang_low', 'Battambang_high', 'Takeo_low', 'Takeo_high', 'Pailin_low', 'Pailin_high']
     
     # book_key_all = ['Pailin_low', 'Pailin_high']
     
@@ -17,11 +18,9 @@ if __name__ == '__main__':
     
     # run the model for all provinces, for low and high baseline incidences
     sc.parallelize(run_me, book_key_all)
-    # with multiprocessing.Pool() as pool:
-    #     pool.map(run_me, book_key_all)
+    sc.parallelize(run_me, book_key_second_half)
     # for prov in book_key_all:
     #     run_me(prov)
-    
     
     # aggregate and plot the results (shown in paper)
     from result_aggregation import run_post_processing
